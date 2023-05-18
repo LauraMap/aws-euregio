237a5b85d5ac2e625609db1e24187983d4ab07ad
LauraMap <laura.saxer@student.uibk.ac.at>
2023-05-18 13:21:52 +0200

    Colour Skala für Schneehöhe hinzugefügt

--- colors.js
+++ colors.js
@@ -102,5 +102,47 @@ const COLORS = {
             max: 999,
             color: "#ff3232"
         },
+    ],
+    snowDepth: [
+        {
+            min: 0,
+            max: 10,
+            color: "#ffffb2"
+        },
+        {
+            min: 10,
+            max: 25,
+            color: "#b0ffbc"
+        },
+        {
+            min: 25,
+            max: 50,
+            color: "#8cffff"
+        },
+        {
+            min: 50,
+            max: 100,
+            color: "#19cdff"
+        },
+        {
+            min: 100,
+            max: 200,
+            color: "#1982ff"
+        },
+        {
+            min: 200,
+            max: 300,
+            color: "#0f5abe"
+        },
+        {
+            min: 300,
+            max: 400,
+            color: "#784bff"
+        },
+        {
+            min: 400,
+            max: 9999,
+            color: "#cd0feb"
+        }
     ]
 }
